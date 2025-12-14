83a43667d16d84caa20edfc63f3f940fc87185a5
Michał Łatyński <michal@latynski.pl>
2025-12-14 23:15:20 +0100

    One serviceworker, link to game

--- sw.js
+++ sw.js
@@ -1,5 +1,8 @@
-const CACHE_NAME = "latynski-pl-cache";
-const FILES_TO_CACHE = [
+var CACHE_VERSION = 'v4';
+var CACHE_PREFIX = 'latynskipl-cache-';
+var CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
+
+var ASSETS = [
 	"/",
 	"/android-chrome-192x192.png",
 	"/android-chrome-512x512.png",
@@ -11,40 +14,60 @@ const FILES_TO_CACHE = [
 	"/profile.jpg",
 	"/script.js",
 	"/site.webmanifest",
-	"/style.css"
+	"/style.css",
+	"/koci_bananiarz/index.html",
+	"/koci_bananiarz/style.css",
+	"/koci_bananiarz/game.js",
+	"/koci_bananiarz/face.jpeg",
+	"/koci_bananiarz/manifest.json"
 ];
 
-// installation and saving files to cache
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(FILES_TO_CACHE);
-    })
-  );
+self.addEventListener('install', function (event) {
   self.skipWaiting();
-});
-
-// activate and clean old cache
-self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.keys().then(keyList => {
-      return Promise.all(
-        keyList.map(key => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      );
+    caches.open(CACHE_NAME).then(function (cache) {
+      return cache.addAll(ASSETS);
     })
   );
-  self.clients.claim();
 });
 
-// Requests handling - cache first, network later
-self.addEventListener("fetch", event => {
+self.addEventListener('activate', function (event) {
+  event.waitUntil(
+    caches.keys().then(function (keys) {
+      var deletions = [];
+      for (var i = 0; i < keys.length; i++) {
+        var k = keys[i];
+        if (k.indexOf(CACHE_PREFIX) === 0 && k !== CACHE_NAME) {
+          deletions.push(caches.delete(k));
+        }
+      }
+      return Promise.all(deletions);
+    }).then(function () {
+      return self.clients.claim();
+    })
+  );
+});
+
+self.addEventListener('fetch', function (event) {
+  // network first when opening PWA app (to force possible updates when new version of one of those files will be available)
+  if (event.request && event.request.mode === 'navigate') {
+    event.respondWith(
+      fetch(event.request).then(function (response) {
+        var copy = response.clone();
+        caches.open(CACHE_NAME).then(function (cache) {
+          cache.put(event.request, copy);
+        });
+        return response;
+      }).catch(function () {
+        return caches.match(event.request);
+      })
+    );
+    return;
+  }
+
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
+    caches.match(event.request).then(function (cached) {
+      return cached || fetch(event.request);
     })
   );
 });
